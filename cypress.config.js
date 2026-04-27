a0a5e37ddb43b058620df02a94bc9c717df86dff
Volodymyr Voloshyn <mailvoloshyn@gmail.com>
2026-04-27 20:32:20 +0300

    Fix: remove allowCypressEnv flag

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: '84t3bz',
-  allowCypressEnv: false,
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'https://telnyx.com',
     specPattern: 'cypress/e2e/**/*.cy.js',
@@ -11,9 +10,7 @@ module.exports = defineConfig({
     defaultCommandTimeout: 10000,
     video: false,
     screenshotOnRunFailure: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
+    setupNodeEvents(on, config) {}
   },
   env: {
     BASE_URL: 'https://telnyx.com'
